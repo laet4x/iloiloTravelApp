66ea1d9ddf3927911b2f49748e713288c9df1260
faavictoriano <victorianofrancis294@gmail.com>
2017-03-29 17:20:02 +0800

    fixed details

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -91,7 +91,7 @@ angular.module('starter.controllers', [])
       img: 'hotel.jpg',
       lat:10.701329, 
       lng:122.567958,
-       desc: {
+      desc: {
               address: '4F The Atrium Mall Gen Luna St, Iloilo City Proper, Iloilo City, 5000 Iloilo',
               website: 'wyndhamhotels.com',
               contact: '(033) 337 3297'
@@ -99,9 +99,9 @@ angular.module('starter.controllers', [])
     }
   ]
   $scope.mapStyle = [{"featureType":"water","stylers":[{"color":"#19a0d8"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"},{"weight":6}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#e85113"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-40}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#efe9e4"},{"lightness":-20}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"road.highway","elementType":"labels.icon"},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","stylers":[{"lightness":20},{"color":"#efe9e4"}]},{"featureType":"landscape.man_made","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"lightness":-100}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"hue":"#11ff00"}]},{"featureType":"poi","elementType":"labels.text.stroke","stylers":[{"lightness":100}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"hue":"#4cff00"},{"saturation":58}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#f0e4d3"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#efe9e4"},{"lightness":-10}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"simplified"}]}]
-  $scope.showMap = function(lat, lng,  desc) {
+  $scope.showMap = function(lat, lng, desc) {
       console.log(lat, lng)
-      $state.go('app.hotel_map', { lat: lat, lng: lng,desc: desc });
+      $state.go('app.hotel_map', { lat: lat, lng: lng, desc: desc });
   }
 }])
 
