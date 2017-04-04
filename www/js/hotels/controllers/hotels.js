8cd74257157d9b774f038ff01322baf2021208f4
faavictoriano <victorianofrancis294@gmail.com>
2017-04-04 15:10:55 +0800

    add location settings

--- www/js/hotels/controllers/hotels.js
+++ www/js/hotels/controllers/hotels.js
@@ -2,50 +2,10 @@
 
 angular
   .module('hotels.module')
-  .controller('HotelsCtrl', ['$scope', '$stateParams', '$state', '$rootScope',
-  function($scope, $stateParams, $state, $rootScope) {
-  $scope.hotels = [
-    { id: 1,
-      title: 'Hotel Del Rio',
-      img: 'del-rio.jpg',
-      pos:[10.700133, 122.552114],
-      desc: {
-              address: 'M.H del Pilar St, Molo, Iloilo City, 5000 Iloilo',
-              website: 'hoteldelrio.com.ph',
-              contact: '(033) 337 0736'
-            }
-    },
-    { id: 2,
-      title: 'Sarabia Manor Hotel' ,
-      img: 'sarabia.jpg',
-      pos:[10.699647,122.556336],
-      desc: {
-              address: 'General Luna Street, Iloilo City Proper, Iloilo City, 5000 Iloilo',
-              website: 'sarabiamanorhotel.com',
-              contact: '(033) 337 0736'
-            }
-    },
-    { id: 3,
-      title: 'Century 21', 
-      img: 'century21.jpg',
-      pos:[10.700421,122.566201],
-      desc: {
-              address: 'Iloilo City Proper, Iloilo City, Iloilo',
-              website: 'ann2.net',
-              contact: '(033) 335 8821'
-            }
-    },
-    { id: 4,
-      title: 'Days Hotel', 
-      img: 'days.jpg',
-      pos:[10.701329, 122.567958],
-      desc: {
-              address: '4F The Atrium Mall Gen Luna St, Iloilo City Proper, Iloilo City, 5000 Iloilo',
-              website: 'wyndhamhotels.com',
-              contact: '(033) 337 3297'
-            }
-    }
-  ]
+  .controller('HotelsCtrl', ['$scope', '$stateParams', '$state', '$rootScope','$http',
+  function($scope, $stateParams, $state, $rootScope,$http) {
+  // The json data will now be in scope.
+   $scope.hotels =  $rootScope.hotels;
   $scope.mapStyle = $rootScope.mapStyle;
   $scope.showMap = function(pos, desc) {
       $state.go('app.hotel_map', { pos: pos, desc: desc });
